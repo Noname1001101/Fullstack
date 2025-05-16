8a9ef030ed83f0328b88977af3940aa9e3c2ab51
Phuoc <phuocmai794@gmail.com>
2025-05-16 14:29:21 +0700

    update

---
+++ JSbasic/video2/video2.js
@@ -0,0 +1,2 @@
+console.log('Hello world Javascript')
+console.log('Hello world Phuoc')
---
+++ JSbasic/video3/video3.js
@@ -0,0 +1,8 @@
+// // let c = 5;
+// // let d = 5.9;
+// // const x = 5;
+// // x = 6;
+// console.log(`tong = `,c == y, x)
+
+let x = 5, y = 6, z= 7;
+console.log(`Test string`,`x= `,x, `y= `, y,`z= `, z);
